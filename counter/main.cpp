546d0860bc0d238287eed48a46481a7f5c2953cd
leandroviajando <65241663+leandroviajando@users.noreply.github.com>
2024-02-23 17:08:16 +0100

    counter

---
+++ counter/main.cpp
@@ -0,0 +1,34 @@
+// Source: https://www.educative.io/courses/learn-cpp-from-scratch
+
+namespace counter {
+
+#include <iostream>
+
+template <class T>
+class Score {
+   private:
+    T value;
+
+   public:
+    static int counter;
+    Score() { counter++; }
+};
+
+template <class T>
+int Score<T>::counter = 0;
+
+// Output:
+// 2
+// 1
+int main() {
+    Score<int> x;
+    Score<int> y;
+    Score<double> z;
+
+    std::cout << Score<int>::counter << std::endl;
+    std::cout << Score<double>::counter << std::endl;
+
+    return 0;
+}
+
+}  // namespace counter
