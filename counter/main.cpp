35ffc8e14573d35381e7468cbd90265a2ef525f2
leandroviajando <65241663+leandroviajando@users.noreply.github.com>
2024-02-23 17:16:43 +0100

    counter

--- counter/main.cpp
+++ counter/main.cpp
@@ -6,29 +6,29 @@ namespace counter {
 
 template <class T>
 class Score {
-   private:
-    T value;
+ private:
+  T value;
 
-   public:
-    static int counter;
-    Score() { counter++; }
+ public:
+  static int counter;
+  Score() { counter++; }
 };
 
 template <class T>
 int Score<T>::counter = 0;
 
+int main() {
+  Score<int> x;
+  Score<int> y;
+  Score<double> z;
+
+  std::cout << Score<int>::counter << std::endl;
+  std::cout << Score<double>::counter << std::endl;
+
+  return 0;
+}
 // Output:
 // 2
 // 1
-int main() {
-    Score<int> x;
-    Score<int> y;
-    Score<double> z;
-
-    std::cout << Score<int>::counter << std::endl;
-    std::cout << Score<double>::counter << std::endl;
-
-    return 0;
-}
 
 }  // namespace counter
